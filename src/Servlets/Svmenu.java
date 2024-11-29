c9db5fb730c5561e986d52e773e34705bf5b2468
Javier <javier.toro2301@alumnos.ubiobio.cl>
2024-11-29 14:56:18 -0300

    menu implmentado v2

--- src/Servlets/Svmenu.java
+++ src/Servlets/Svmenu.java
@@ -6,24 +6,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet(name = "Svmenu", urlPatterns = {"/Svmenu"})
 public class Svmenu extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int Ccompleto = Integer.parseInt(req.getParameter("completoCantidad"));
-        int Chamburguesa = Integer.parseInt(req.getParameter("hamburguesaCantidad"));
-        int Cpizza=Integer.parseInt(req.getParameter("pizzaCantidad"));
-        int CpapasFritas=Integer.parseInt(req.getParameter("papasFritasCantidad"));
-        int Csushi=Integer.parseInt(req.getParameter("sushiCantidad"));
-        int Cbowl=Integer.parseInt(req.getParameter("bowlCantidad"));
+        HashMap<String, Integer> menu = new HashMap<>();
 
-        System.out.println("Completo: " + Ccompleto);
-        System.out.println("Hamburguesa: " + Chamburguesa);
-        System.out.println("Pizza: " + Cpizza);
-        System.out.println("Papas Fritas: " + CpapasFritas);
-        System.out.println("Sushi: " + Csushi);
-        System.out.println("Bowl: " + Cbowl);
+        String[] carta={"completo","hamburguesa","pizza","papasFritas","sushi","bowl"};
+
+        for (int i = 0; i < carta.length; i++) {
+            menu.put(carta[i], Integer.parseInt(req.getParameter(carta[i]+"Cantidad")));
+        }
+
+        for (String key : menu.keySet()) {
+            System.out.println( key + ", cantidad :" + menu.get(key));
+        }
+
 
         resp.sendRedirect("menuPrincipal.jsp");
     }
